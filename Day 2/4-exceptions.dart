75b75404df4ca32e67811351e71b18b6d613c59b
rps_naik <36451695+rpsnaik@users.noreply.github.com>
2020-04-10 17:21:28 +0530

    Update 4-exceptions.dart

--- Day 2/4-exceptions.dart
+++ Day 2/4-exceptions.dart
@@ -1,8 +1,14 @@
-main(){
+void main(){
+  int a = 10;
+  int b = 0;
+  
+  int c;
+  
   try{
-    int c = 45~/0;
-    print(c);
+    c = a~/b;
   }catch(e){
     print(e);
+    print("You cannot perfrome that operation!");
   }
+  print(c);
 }
