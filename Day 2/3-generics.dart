cfeabfaa33204ee4bff61ad92dd4d93eff86dd64
rps_naik <36451695+rpsnaik@users.noreply.github.com>
2020-04-10 17:21:07 +0530

    Update 3-generics.dart

--- Day 2/3-generics.dart
+++ Day 2/3-generics.dart
@@ -1,5 +1,28 @@
-main(){
-  List<String> data = ["R P S Naik", "John", "Peter", "Ajay"];
+class Student{
+  String name;
+  int rollNo;
+  String sec;
+  int marks;
+  
+  void addData(Student stu1){
+    stu1.name = "R P S Naik";
+    stu1.rollNo = 23;
+    stu1.sec = "10th Standard";
+    stu1.marks = 82;
+  }
+  
+  void showData(Student stu1){
+    print(stu1.name);
+    print(stu1.rollNo);
+    print(stu1.sec);
+    print(stu1.marks);
+  }
+  
+  
+}
 
+main(){
+  List<Student> data = [];
+  
   print(data);
 }
